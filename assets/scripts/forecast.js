b5825b36da28bfab8e43a1f7591257fd5b7c290f
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-04-02 22:46:45 +0200

    Get City API Call

--- assets/scripts/forecast.js
+++ assets/scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = '5zCHSaYDeNYUaK0EBO9NLjI0J1UATZKC';
+
+const  getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+getCity('manchester')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
