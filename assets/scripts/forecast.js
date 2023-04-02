a50b53b8ae12f9a6226e9f08a2e847d3462aff17
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-04-02 23:38:15 +0200

    Get Weather API Call

--- assets/scripts/forecast.js
+++ assets/scripts/forecast.js
@@ -1,5 +1,25 @@
 const key = '5zCHSaYDeNYUaK0EBO9NLjI0J1UATZKC';
 
+
+
+
+
+// get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+
+
+
+// get city information
 const  getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +32,9 @@ const  getCity = async (city) => {
 
 };
 
-getCity('manchester')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+
+getCity('manchester').then(data => {
+    return getWeather(data.Key);
+  }).then(data => {
+    console.log(data);
+  }).catch(err => console.log(err));   
