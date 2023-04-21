ea4a1e04b339d16d7906660fcea61f50115bcf57
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-04-21 19:26:34 +0200

    icons & images

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
     // const cityDets = data.cityDets;
@@ -19,6 +21,27 @@ const updateUI = (data) => {
            <span>&deg;C</span>
         </div>
     `;
+
+
+
+     
+    // update the night/day & ico images
+
+    const iconSrc = `assets/img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+
+    let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'assets/img/day.svg';
+    }else{
+        timeSrc = 'assets/img/night.svg';
+    }
+
+    time.setAttribute('src', timeSrc);
+
+
+
 
     // remove the d-none class if present
     if(card.classList.contains('d-none')){
